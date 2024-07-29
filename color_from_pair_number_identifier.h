fe1f05331b0ebb4985d8c50bd27fb4e32c8fa940
JEEVAVASANTHAN-1045 <jiivavasanthan@gmail.com>
2024-07-29 10:37:58 +0530

    Create color_from_pair_number_identifier.h

---
+++ color_from_pair_number_identifier.h
@@ -0,0 +1,6 @@
+#ifndef GET_COLOR_FROM_PAIR_NUMBER_IDENTIFIER_H
+#define GET_COLOR_FROM_PAIR_NUMBER_IDENTIFIER_H
+#include "types_of_colour_code"
+
+ColorPair GetColorFromPairNumber(int pairNumber);
+#endif
